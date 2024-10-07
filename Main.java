27c838294a046074336b6c57c4c256c4b78a2add
unorderlyhamster329 <mattng08@gmail.com>
2024-10-07 12:13:53 -0700

    dwabdwahdwa

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main
 {
   public static void main(String[] args)
   {
+    //Problem 1
     Circle circ1 = new Circle(10.1);
     Circle circ2 = new Circle(14);
     Circle circ3 = new Circle(20.5);
@@ -17,7 +18,37 @@ public class Main
     String output3 = circ3.toString();
     System.out.println(output3);
 
-    Scanner banner = new Scanner(System.in);
+    //Problem 2
+    Scanner rectInput = new Scanner(System.in);
     System.out.println("Type a number for length and width");
+    double lengthandWidth = rectInput.nextDouble();
+    System.out.println("Type a length");
+    double length = rectInput.nextDouble();
+    System.out.println("Type a width");
+    double width = rectInput.nextDouble();
+
+    Rectangle rect1 = new Rectangle(lengthandWidth);
+    Rectangle rect2 = new Rectangle(length, width);
+
+    String output4 = rect1.toString();
+    System.out.println(output4);
+    
+    String output5 = rect2.toString();
+    System.out.println(output5);
+
+
+    //Problem 3
+    System.out.println("Type a side length");
+    double sLength = rectInput.nextDouble();
+
+    RegularPolygon triangle = new RegularPolygon(3, sLength);
+    RegularPolygon square = new RegularPolygon(4, sLength);
+    String output6 = triangle.toString();
+    System.out.println(output6);
+    String output7 = square.toString();
+    System.out.println(output7);
+
+    
+    
   }
 }
