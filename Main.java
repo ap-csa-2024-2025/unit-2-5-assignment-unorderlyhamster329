cdd891c6ae01c75fb79b195dce2009015f2da9cb
unorderlyhamster329 <mattng08@gmail.com>
2024-10-07 11:30:09 -0700

    jj\

--- Main.java
+++ Main.java
@@ -4,6 +4,20 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Circle circ1 = new Circle(10.1);
+    Circle circ2 = new Circle(14);
+    Circle circ3 = new Circle(20.5);
+
+    String output1 = circ1.toString();
+    System.out.println(output1); 
+
+    String output2 = circ2.toString();
+    System.out.println(output2);
+
+    String output3 = circ3.toString();
+    System.out.println(output3);
+
+    Scanner banner = new Scanner(System.in);
+    System.out.println("Type a number for length and width");
   }
 }
